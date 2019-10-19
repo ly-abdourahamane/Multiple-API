b31ddd323cb1b083ed5ed7a4573d15b5c31ff873
Abdou <abdourahamane.ly1@gmail.com>
2019-10-20 00:12:47 +0200

    Config jwt token security

---
+++ src/main/java/com/mandat/amoulanfe/security/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.mandat.amoulanfe.security;
+
+import com.mandat.amoulanfe.exception.NotFoundException;
+import com.mandat.amoulanfe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.mandat.amoulanfe.domain.User;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new NotFoundException("User not found [email: " + email + "]")
+                );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("User not found [id: " + id + "]")
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
