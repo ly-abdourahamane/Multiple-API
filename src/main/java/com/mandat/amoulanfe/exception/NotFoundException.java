47f771d4d9946fa3754f7e97fe495cc86255df2e
Abdou <abdourahamane.ly1@gmail.com>
2019-10-20 00:16:32 +0200

    Add spciale exceptions

---
+++ src/main/java/com/mandat/amoulanfe/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.mandat.amoulanfe.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
