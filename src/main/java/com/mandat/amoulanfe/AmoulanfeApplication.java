4ddfaf0e2a4515b908dfea89d158062a6826ff23
Abdou <abdourahamane.ly1@gmail.com>
2019-10-20 00:10:34 +0200

    Initial commit

---
+++ src/main/java/com/mandat/amoulanfe/AmoulanfeApplication.java
@@ -0,0 +1,12 @@
+package com.mandat.amoulanfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoulanfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmoulanfeApplication.class, args);
+	}
+}
