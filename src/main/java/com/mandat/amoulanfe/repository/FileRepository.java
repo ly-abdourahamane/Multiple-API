a73ffeff0f9e4dee2c544cbf100671dfca0a1249
Abdou <abdourahamane.ly1@gmail.com>
2020-04-27 13:57:07 +0200

    To use spring find by name

--- src/main/java/com/mandat/amoulanfe/repository/FileRepository.java
+++ src/main/java/com/mandat/amoulanfe/repository/FileRepository.java
@@ -3,7 +3,6 @@ package com.mandat.amoulanfe.repository;
 import com.mandat.amoulanfe.domain.FileDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,8 +13,7 @@ public interface FileRepository extends JpaRepository<FileDomain, Long> {
     @Query(value = "SELECT f FROM FileDomain f")
     List<FileDomain> getAllFiles();
 
-    @Query(value = "SELECT f FROM FileDomain f WHERE f.name = :name")
-    FileDomain findByName(@Param("name") String name);
+    FileDomain findByName(String name);
 
     Boolean existsByName(String name);
 }
